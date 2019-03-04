bc4d4eb06ec1a669d7f2893222adf8956f90c635
Ronit Basu <ronitbasu1234@gmail.com>
2019-03-03 22:44:35 -0600

    closes #3

--- src/App.js
+++ src/App.js
@@ -1,28 +1,80 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import data from './static/data/products';
+import PropTypes from 'prop-types';
+
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="main">
+        <Cart />
+        <ProductCatalog products={data.products} />
       </div>
     );
   }
 }
+
+class Cart extends Component {
+  render() {
+    return (
+      <div className="float-cart">
+        <img src="shopping-cart.png" styles={{float: "right"}} alt="cart"/>
+      </div>
+    );
+  }
+}
+
+class ProductCatalog extends Component {
+  render() {
+    let products = this.props.products;
+    var cols = [];
+    products.forEach((product) => {
+      cols.push(<ProductCol product={product} key={product.name} />);
+    });
+  return (
+    <div className="shelf-container">
+      {cols}
+    </div>
+  );
+  }
+}
+
+class ProductCol extends Component {
+  render() {
+    let product = this.props.product;
+    return (
+      <div className="shelf-item">
+        <Thumb
+          classes="shelf-item__thumb"
+          src={require(`./static/images/${product.sku}_2.jpg`)}
+          alt={product.title}
+        />
+        <div className="shelf-item__details">
+          <p className="title">{product.title}</p>
+        </div>
+        <div className="shelf-item__price">
+          <p>{`${product.currencyFormat}  ${product.price}`}</p>
+        </div>
+        <div className="shelf-item__buy-btn">Add to cart</div>
+      </div>
+    );
+  }
+}
+
+const Thumb = props => {
+  return (
+    <div className={props.classes}>
+      <img src={props.src} alt={props.alt} title={props.title} />
+    </div>
+  );
+};
+
+Thumb.propTypes = {
+  alt: PropTypes.string,
+  title: PropTypes.string,
+  classes: PropTypes.string,
+  src: PropTypes.string.isRequired
+};
 
 export default App;
